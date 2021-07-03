44f1dae0446db6890c4e34edf2071500bfa57209
PigeonsHouse <bullbull1230@gmail.com>
2021-07-03 20:59:51 +0900

    [add] コメントのViewと折りたたみ機能の実装

--- components/work/Comment.vue
+++ components/work/Comment.vue
@@ -1,39 +1,57 @@
 <template>
   <div class="comment">
     <h3 class="title">コメント</h3>
-    <form @submit.prevent="submitComment">
+    <form class="comment-form-wrapper" @submit.prevent="submitComment">
 			<label>name</label>
-			<input name="author" type="text" class="username_form" v-model="author_name" />
+			<input name="author" type="text" class="username-form" v-model="author_name" />
 			<label>comment</label>
 			<div class="send">
 				<textarea name="comments" type="text" class="comment-form" required rows="4" v-model="comment"></textarea>
 				<input type="image" src="/send.png" height="20px" width="20px"/>
 			</div>
     </form>
+		<div>
+			<button class="comment-list-opener" @click="test">{{ is_open_icon }}</button>
+			<comment v-if="is_open" :comment_list="comments"/>
+		</div>
   </div>
 </template>
 
 <script>
+import CommentCard from './_comment.vue'
+
 export default {
+	components: {
+		comment: CommentCard,
+	},
 	data() {
 		return {
 			author_name: "Guest",
 			comment: "",
-			comments: [
-				{
-					author_name: "hoge",
-					comment: "すごいですね"
-				},
-				{
-					author_name: "fuga",
-					comment: "8888"
-				}
-			]
+			is_open: false,
+			is_open_icon_list: ['∨', '∧'],
+			is_open_icon: '∨',
+			comments: []
 		}
 	},
 	methods: {
+		test(){
+			this.is_open = !this.is_open;
+			this.is_open_icon = this.is_open_icon_list[this.is_open ? 1 : 0];
+		},
 		submitComment() {
 			console.log(this.author_name, this.comment);
+			this.comments = 
+			[
+				...this.comments,
+				{
+					id: this.comments.length,
+					author_name: this.author_name,
+					comment: this.comment,
+					created_at: "2021/07/03"
+				}
+			]
+			this.comment = ''
 
 		}
 	},
@@ -44,12 +62,15 @@ export default {
 label{
 	display: block;
 }
+.comment-form-wrapper {
+	margin-bottom: 20px;
+}
 .comment {
   width: 80%;
 	max-width: 650px;
   margin: auto;
 }
-.username_form{
+.username-form {
 	width: 40%;
 	margin-bottom: 10px;
   height: 80%;
@@ -57,7 +78,7 @@ label{
   outline: none;
 	border-bottom: 1px solid #c2c2c2;
 }
-.send{
+.send {
 	display: flex;
 	align-items: flex-end;
 }
@@ -69,5 +90,12 @@ label{
   outline: none;
 	border-bottom: 1px solid #c2c2c2;
 	margin-right: 10px;
+}
+.comment-list-opener{
+	width: 100%;
+	border-radius: 3px;
+	border: 1px #c2c2c2 solid;
+	background-color: #0000;
+	font-weight: bold;
 }
 </style>
---
+++ components/work/_comment.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<div v-for="comment_data in comment_list" v-bind:key="comment_data.id">
+			<div class="comment-component">
+				<div class="comment-info">
+					<h3 class="username-view">{{ comment_data.author_name }}</h3>
+					<p class="comment-view">{{ comment_data.comment }}</p>
+				</div>
+				<div class="comment-timeinfo">
+					<small>{{ comment_data.created_at }}</small>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		comment_list: {
+			default: []
+		}
+	}
+}
+</script>
+
+<style lang="css" scoped>
+.comment-component {
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+}
+.comment-info {
+	max-width: 100%;
+}
+</style>
