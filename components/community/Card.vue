0dedca24ecae5a3b50a92f38d61d8416b40186cd
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-07-04 22:19:25 +0900

    [add]　#2 Card.vueとCardList.vueの追加およびデザインの作成

---
+++ components/community/Card.vue
@@ -0,0 +1,72 @@
+<template>
+  <article class="card">
+    <img src="../../assets/smple_img.png" alt="thumbnail" title="thumbnail" class="thumbnail"/>
+    <div class="content">
+        <div class="title">{{title}}</div>
+        <div class="description">{{description}}</div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "hoge",
+    },
+    team: {
+      type: Object,
+      required: true
+    },
+    tags: {
+      type: Array,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      default: "enpty"
+    },
+    card_image: {
+      type: String,
+      required: true,
+      default: "enpty"
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+    width: 200px;
+    height: 300px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.24);
+}
+.content {
+    position: relative;
+    width: 200px;
+    height: 140px;
+    padding-left: 5px;
+    padding-right: 5px;
+    overflow: hidden;
+}
+.thumbnail {
+    position: relative;
+    width: 200px;
+    height: 150px;
+    object-fit: cover;
+}
+.title {
+    font-size: 20px;
+    font-weight: bold;
+}
+.description {
+    position: relative;
+    /* overflow-wrap: break-word; */
+    word-break: break-all;
+    
+}
+</style>
---
+++ components/community/CardList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card-list">
+    <card
+      class="card"
+      v-for="card_item in card_items"
+      :title="card_item.title"
+      :team="card_item.team"
+      :tags="card_item.tags"
+      :description="card_item.description"
+      :card_image="card_item.card_image"
+      :key="card_item.id"
+    />
+  </div>
+</template>
+
+<script>
+import Card from "./Card"
+
+export default {
+  components: { Card },
+  props: {
+    card_items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
