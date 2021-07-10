50c481ff9c12861ac0d1c4f92dae1a3e59dbffc4
citrus-candy <kyuuri11013@gmail.com>
2021-07-10 23:22:44 +0900

    [fix] コンポーネントの構造を変更

---
+++ components/top/CommunitiesHero.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="communities-hero">
+    <div v-for="(item, i) in items" :key="i">
+      <community-hero :item="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CommunityHero from "./CommunityHero.vue";
+
+export default {
+  components: { CommunityHero },
+  props: {
+    items: {
+      type: Object,
+      required: true,
+      default: [
+        {
+          name: "GAME",
+          description: "GAMEコミュの説明",
+          imgUrl: "",
+          linkUrl: ""
+        },
+        {
+          name: "CG",
+          description: "CGコミュの説明",
+          imgUrl: "",
+          linkUrl: ""
+        },
+        {
+          name: "HACK",
+          description: "HACKコミュの説明",
+          imgUrl: "",
+          linkUrl: ""
+        },
+        {
+          name: "MEDIA ART",
+          description: "MEDIA ARTコミュの説明",
+          imgUrl: "",
+          linkUrl: ""
+        }
+      ]
+    }
+  },
+  data: () => ({
+    items: []
+  })
+};
+</script>
+
+<style scoped></style>
--- components/top/CommunityHero.vue
+++ components/top/CommunityHero.vue
@@ -1,52 +1,68 @@
 <template>
-  <div class="communityHero">
-    <div v-for="(item, i) in items" :key="i">
-      <game-hero
-        :name="item.name"
-        :imgUrl="item.imgUrl"
-        :description="item.description"
-      />
-    </div>
+  <div class="community-hero" :style="styles">
+    <h1>{{ item.name }}</h1>
+    <div class="description">{{ item.description }}</div>
+    <a :href="item.linkUrl">
+      <span> もっとみる→ </span>
+    </a>
   </div>
 </template>
 
 <script>
-import CGHero from "./CGHero.vue";
-import GameHero from "./GameHero.vue";
-import HackHero from "./HackHero.vue";
-import MediaArtHero from "./MediaArtHero.vue";
-
 export default {
-  components: { GameHero, CGHero, HackHero, MediaArtHero },
-  data: () => ({
-    items: [
-      {
-        name: "GAME",
-        description: "GAMEコミュの説明",
-        imgUrl: "",
-        linkUrl: "",
-      },
-      {
-        name: "CG",
-        description: "CGコミュの説明",
-        imgUrl: "",
-        linkUrl: "",
-      },
-      {
-        name: "HACK",
-        description: "HACKコミュの説明",
-        imgUrl: "",
-        linkUrl: "",
-      },
-      {
-        name: "MEDIA ART",
-        description: "MEDIA ARTコミュの説明",
-        imgUrl: "",
-        linkUrl: "",
-      },
-    ],
-  }),
+  props: {
+    item: {
+      type: Object,
+      required: true,
+      default: {
+        name: "Not Found",
+        description: "説明をここに記載",
+        imgUrl: "https://placehold.jp/1920x1080.png",
+        linkUrl: ""
+      }
+    }
+  },
+  computed: {
+    styles() {
+      return {
+        "--bgUrl": "url(" + this.item.imgUrl + ")"
+      };
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.community-hero {
+  background-image: var(--bgUrl);
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin: 0;
+  padding: 40px;
+  height: 100vh;
+}
+h1 {
+  font-size: 150px;
+  opacity: 0.5;
+  color: white;
+}
+.description {
+  color: white;
+  font-size: 1.5rem;
+  margin: 100px 0 100px 40%;
+}
+span {
+  text-align: center;
+}
+a {
+  color: white;
+  border: solid white;
+  border-radius: 50px;
+  padding: 10px 50px;
+  text-decoration: none;
+  margin: auto;
+  width: fit-content;
+}
+</style>
