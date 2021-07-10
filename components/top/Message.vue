2d0e91a33fbe3530b344646b5b169539d674a2b9
zatsu <fuwafumikanofu@gmail.com>
2021-07-10 19:29:43 +0900

    [add] #6

--- components/top/Message.vue
+++ components/top/Message.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <h1>
-      {{ sample }}
-    </h1>
+    <div class="box1">
+      <h1>
+        Message
+      </h1>
+      <p style="text-align: left">皆さんこんにちは。うんぬんかんぬんああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ万歳！まんせーまんせー！！！うぇ～～～～～～～～～～～～～～～～～～～～～～～～～～～＾い！！みんな見てる～～～～～～～～～？？？？？？？？？？？？？？？？？？？？？？？？？？？wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</p>
+    </div>
   </div>
 </template>
 
@@ -19,5 +22,25 @@ export default {
 </script>
 
 <style scoped>
-    
+  .box1 {
+    padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+  }
+  .box1 p {
+    padding-top: 50px;
+    font-size: 90%;
+    width:95%;
+    height:600px;
+    padding-left: 25%;
+    padding-right: 8%;
+  }
+  .box1 h1 {
+    border-bottom: solid 35px #fcff5d;
+    color: #2f4875;
+    font-size: 500%;
+    height:100px;
+    width:420px;
+    padding-left: 5%;
+  }
 </style>
