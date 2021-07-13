6ee7959207fec68e2bd098295c12f2d016a04c0c
rkun123 <tren111213@gmail.com>
2021-07-13 15:48:10 +0000

    [hotfix] remove @nuxtjs/fontawesome buildModule

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/fontawesome'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
