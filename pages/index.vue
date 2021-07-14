1b5b741f8333867e88e9af09c574ab8c6079d9da
citrus-candy <kyuuri11013@gmail.com>
2021-07-15 01:57:52 +0900

    [add] トップページの仮完成 #50

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,63 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        OpenCampus2021
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <top />
+    <about-c-3 />
+    <communities-hero :items="items" />
+    <message />
   </div>
 </template>
 
 <script>
-export default {}
+import AboutC3 from "../components/top/AboutC3.vue";
+import CommunitiesHero from "../components/top/CommunitiesHero.vue";
+import Message from "../components/top/Message.vue";
+import Top from "../components/top/Top.vue";
+
+export default {
+  components: { Top, AboutC3, CommunitiesHero, Message },
+  data() {
+    return {
+      items: [
+        {
+          name: "GAME",
+          description:
+            "ゲーム開発（ゲームかいはつ、英: Game development）とは、ゲームの開発までの制作過程のことをいう。",
+          imgUrl:
+            "https://unity3d.com/profiles/unity3d/themes/unity/images/unity/beta/alpha-beta/2020.2b-improvements.jpg",
+          linkUrl: "/community/1"
+        },
+        {
+          name: "CG",
+          description:
+            "コンピュータグラフィックス（英: computer graphics、略称: CG）は、コンピュータを用いて画像を生成する科学技術、及びその技術によって生成される画像のことである。",
+          imgUrl:
+            "https://assets.st-note.com/production/uploads/images/42744060/picture_pc_2bf81205a93c3e77d4108e7ffbedaaa7.png?width=800",
+          linkUrl: "/community/2"
+        },
+        {
+          name: "HACK",
+          description:
+            "コンピュータープログラミング（英語: Computer Programming）とは、ある特定のコンピューティングの結果を得ることを目的として、実行可能なコンピュータープログラムを設計・構築するプロセスのことである。",
+          imgUrl:
+            "https://startupcafe-ku.osaka/wp/wp-content/uploads/2020/03/florian-olivo-4hbJ-eymZ1o-unsplash-1200-1024x683.jpg",
+          linkUrl: "/community/3"
+        },
+        {
+          name: "MEDIA ART",
+          description:
+            "ニューメディアアート、メディアアート（New media art, media art）は、20世紀中盤より広く知られるようになった、芸術表現に新しい技術的発明を利用する、もしくは新たな技術的発明によって生み出される芸術の総称的な用語である。",
+          imgUrl:
+            "https://i0.wp.com/necojita.com/wordpress/wp-content/uploads/2017/11/Image-2017-11-13-9-40.jpeg?resize=1276%2C718",
+          linkUrl: "/community/4"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
