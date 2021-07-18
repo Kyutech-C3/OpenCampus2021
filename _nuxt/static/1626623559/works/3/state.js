f80a8fe62b87670dbbb03f7cdb27785bc02f4f0c
Simo-C3 <Simo-C3@users.noreply.github.com>
2021-07-18 15:52:42 +0000

    deploy: 1abade5b28cfcc91b09a6181429db22797cb8637

---
+++ _nuxt/static/1626623559/works/3/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626623559",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002F3",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
