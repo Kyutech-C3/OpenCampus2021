d998f597afa766f5d47bf4b809d760382121765f
Prokuma <Prokuma@users.noreply.github.com>
2021-07-18 11:50:20 +0000

    deploy: 6edff952d1bf0bb311226babd9346fb616e88cce

--- _nuxt/static/1626608891/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FOpenCampus2021\u002F_nuxt\u002Fstatic\u002F1626608891",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FOpenCampus2021\u002F",assetsPath:"\u002FOpenCampus2021\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1626608891/works/1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FOpenCampus2021\u002F_nuxt\u002Fstatic\u002F1626608891",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002F1",config:{_app:{basePath:"\u002FOpenCampus2021\u002F",assetsPath:"\u002FOpenCampus2021\u002F_nuxt\u002F",cdnURL:a}}}}(null));
