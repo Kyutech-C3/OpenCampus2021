10ec2aedee945139842cba9cab6a943c69909305
rkun123 <tren111213@gmail.com>
2021-07-19 06:11:19 +0000

    [debug]

--- plugins/ghpagespathsolver.js
+++ plugins/ghpagespathsolver.js
@@ -1,8 +1,10 @@
 export default (context, inject) => {
 	const query = window.location.search
 	console.debug('GitHub Pages Path Solver Loaded...')
+	console.debug('query:', query)
 	if ( query.startsWith('?p=') ) {
 		const route = decodeURIComponent(query.substr(3))
+		console.debug('route:', route)
 		window.history.replaceState(null, null, route)
 	}
 }
