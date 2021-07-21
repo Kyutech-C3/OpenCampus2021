b62b255513389f4e29e296fc8a63b76346c0a75d
catgolem <kugi1255@icloud.com>
2021-07-21 23:01:03 +0900

    [update] #250 新しいコミュニティ画像に差し替えた

--- pages/index.vue
+++ pages/index.vue
@@ -29,7 +29,7 @@ export default {
           description:
             "ゲームコミュニティではアクション、シューティング、ノベルゲームなど様々なジャンルのゲームを制作・公開しています。CGやメディアアートコミュニティのメンバーと協力して制作することもあります。ゲーム制作で分からないことがあったら先輩や仲間に聞ける良い環境です。",
           imgUrl:
-            "https://cdn.discordapp.com/attachments/840484770144059462/867038141642309642/unknown-1.png",
+            "https://simo-c3.github.io/image_url/Game.png",
           linkUrl: "/community/2"
         },
         {
@@ -37,7 +37,7 @@ export default {
           description:
             "CGコミュニティではBlenderやXISMOなどのモデリングソフトを用いて3DモデルやMMDでアニメーションを制作しています。",
           imgUrl:
-            "https://cdn.discordapp.com/attachments/840484770144059462/867038326107799562/20210309.jpg",
+            "https://simo-c3.github.io/image_url/CG.png",
           linkUrl: "/community/3"
         },
         {
@@ -45,7 +45,7 @@ export default {
           description:
             "Hackコミュニティは、元々ProgrammingコミュニティーとWebコミュニティに分かれていたものが合体してできたコミュニティーです。現在は、Web制作やアプリ開発、CPU開発、Bot作成など様々な領域で開発に取り組んでいます。このサイトで展示されている作品の中には、大学生になってパソコンを持った子が制作した作品もあります。",
           imgUrl:
-            "https://media.discordapp.net/attachments/840484770144059462/867038102093430824/2021-03-01.png?width=1410&height=793",
+            "https://simo-c3.github.io/image_url/hack.jpg",
           linkUrl: "/community/1"
         },
         {
@@ -53,7 +53,7 @@ export default {
           description:
             "メディアアートコミュニティではAfter EffectやPremiere Proなどでの動画編集・制作やStudio Oneでの音楽制作、Clip Studioでのイラスト制作を行っております！",
           imgUrl:
-            "https://cdn.discordapp.com/attachments/840484770144059462/867038115867525120/c9fbc6dffe394fc6.JPG",
+            "https://simo-c3.github.io/image_url/MediaArt.jpg",
           linkUrl: "/community/4"
         }
       ]
