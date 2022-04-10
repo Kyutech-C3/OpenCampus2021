37f213b27e4d575281981fb0d893478a282de553
Simo-C3 <Simo-C3@users.noreply.github.com>
2022-04-10 12:53:48 +0000

    deploy: 9de4bf708dd237a6558827d3a8fc1eb1d96fefdc

--- _nuxt/7e6eccc.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{388:function(e,n,t){"use strict";t.r(n);var l={props:{sample:{type:String,required:!0,default:"not found"}}},r=t(12),component=Object(r.a)(l,(function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("\n    "+e._s(e.sample)+"\n  ")])])}),[],!1,null,null,null);n.default=component.exports}}]);
